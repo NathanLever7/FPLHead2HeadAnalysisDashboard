9927dbf0817186e7f62cb65c541376945a1babad
NathanLever7 <129379760+NathanLever7@users.noreply.github.com>
2023-09-17 10:18:59 +0100

    Update H2H_Dashboard.py

--- H2H_Dashboard.py
+++ H2H_Dashboard.py
@@ -23,7 +23,7 @@ points_per_run = response.json()
 st.title('FPL Head2Head Analysis')
 
 # Display dataframes as tables
-st.header('Average Position')
+st.header('CHANGE PLEASE')
 st.write(total_position)
 
 st.header('Times in Position')
